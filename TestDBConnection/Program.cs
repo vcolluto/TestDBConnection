fa3d9c57d151b440f52fdc9e63fd041edafb17b1
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-20 10:53:40 +0200

    versione con parametri

--- TestDBConnection/Program.cs
+++ TestDBConnection/Program.cs
@@ -11,35 +11,79 @@ userName = Console.ReadLine();
 Console.Write("Inserisci la tua password: ");
 password = Console.ReadLine();
 
+loginSenzaInjection(userName,password);
 
-// istanzio la risorsa nello using
-using (SqlConnection connessioneSql = new SqlConnection(connectionString))
+
+static void loginConInjection(string userName, string password)
 {
-    // da qui in poi posso usare la risorsa 
-    try
+    // istanzio la risorsa nello using
+    using (SqlConnection connessioneSql = new SqlConnection(connectionString))
     {
-        connessioneSql.Open();
-       // Console.WriteLine("Connessione effettuata!");
-
-        string sqlQuery = $"SELECT FirstName, LastName FROM Employees " +
-                            $"WHERE UserName='{userName}' AND password='{password}'";       //DA EVITARE COME LA PESTE!
-        // ad esempio, provare ad inserire come password: ' or 1=1 --
-        using (SqlCommand cmd=new SqlCommand(sqlQuery, connessioneSql))
-        using (SqlDataReader reader = cmd.ExecuteReader())
+        // da qui in poi posso usare la risorsa 
+        try
         {
-            if (reader.Read())  //ho trovato una riga     
-            {
-                Console.WriteLine($"Benvenuto {reader["FirstName"]} {reader["LastName"]}");
-                Console.WriteLine("Esegui operazioni riservate!!!");
-            }
+            connessioneSql.Open();
+            // Console.WriteLine("Connessione effettuata!");
 
-            else
-                Console.WriteLine("Username o password errati!");
+            string sqlQuery = $"SELECT FirstName, LastName FROM Employees " +
+                                $"WHERE UserName='{userName}' AND password='{password}'";       //DA EVITARE COME LA PESTE!
+                                                                                                // ad esempio, provare ad inserire come password: ' or 1=1 --
+            using (SqlCommand cmd = new SqlCommand(sqlQuery, connessioneSql))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())  //ho trovato una riga     
+                {
+                    Console.WriteLine($"Benvenuto {reader["FirstName"]} {reader["LastName"]}");
+                    Console.WriteLine("Esegui operazioni riservate!!!");
+                }
+
+                else
+                    Console.WriteLine("Username o password errati!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
         }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.ToString());
-    }
-    // non serve usare finally per chiudere la risorsa
 }
+
+    static void loginSenzaInjection(string userName, string password)
+    {
+        // istanzio la risorsa nello using
+        using (SqlConnection connessioneSql = new SqlConnection(connectionString))
+        {
+            // da qui in poi posso usare la risorsa 
+            try
+            {
+                connessioneSql.Open();
+                // Console.WriteLine("Connessione effettuata!");
+
+                string sqlQuery = $"SELECT FirstName, LastName FROM Employees " +
+                                    $"WHERE UserName=@UserName AND password=@password";       //non ho usato la concatenazione di stringhe
+                                                                                                    
+                using (SqlCommand cmd = new SqlCommand(sqlQuery, connessioneSql))
+                {
+                    cmd.Parameters.AddWithValue("@UserName", userName);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())  //ho trovato una riga     
+                        {
+                            Console.WriteLine($"Benvenuto {reader["FirstName"]} {reader["LastName"]}");
+                            Console.WriteLine("Esegui operazioni riservate!!!");
+                        }
+
+                        else
+                            Console.WriteLine("Username o password errati!");
+                    }
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
