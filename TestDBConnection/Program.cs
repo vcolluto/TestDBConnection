7afa4cb9c9f912a2201b3bfd150f8b8e163e69c4
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-20 09:48:43 +0200

    Add project files.

---
+++ TestDBConnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+const string connectionString = 
+    "Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True";
+
+
+// istanzio la risorsa nello using
+using (SqlConnection connessioneSql = new SqlConnection(connectionString))
+{
+    // da qui in poi posso usare la risorsa 
+    try
+    {
+        connessioneSql.Open();
+        Console.WriteLine("Connessione effettuata!");
+
+        string sql
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+    // non serve usare finally per chiudere la risorsa
+}
